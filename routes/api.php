8c61ff522a51e134d8d9497253f38ae07c42b02f
A Chernykh <nadycher@gmail.com>
2020-07-21 10:51:53 +0300

    Create api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/reviews/all', function () {
+    return 'все';
+});
+Route::get('/reviews/{id}', function ($id) {
+    return 'один-'.$id;
+});
+Route::post('/reviews/new', function () {
+    return 'новый';
 });
